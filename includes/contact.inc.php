935708be89ceb366f10fcc18928174b8dda5ebd8
Christopher Lemaitre <kriss7670@gmail.com>
2019-12-11 12:32:00 +0100

    formulaire tableau

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,11 +1,54 @@
 <h1>Pour nous contacter</h1>
 <?php
 if (isset($_POST['frmContact'])) {
-  echo "Je viens du formulaire";
+  $nom = checkinput($_POST['nom']);
+  $prenom = checkinput($_POST['prenom']);
+  $mail = checkinput($_POST['mail']);
+  $msg = checkinput($_POST['msg']);
+
+$erreur = array();
+
+if ($nom === "")
+array_push($erreur, "veuillez saisir votre nom");
+
+if ($prenom === "")
+array_push($erreur, "veuillez saisir votre prenom");
+
+
+if ($mail === "")
+array_push($erreur, "veuillez saisir votre mail");
+
+
+if ($msg === "")
+array_push($erreur, "veuillez saisir votre message");
+
+
+if (count($erreur) > 0) {
+  $message = '<ul>';
+
+  for($i = 0 ; $i < count($erreur) ; $i++) {
+      $message .= '<li>';
+      $message .= $erreur[$i];
+      $message .= '</li>';
+  }
+
+
+
+  $message .= '</ul>';
+
+  echo $message;
+
+  require 'frmContact.php';
+
 }
+
+
 
 else {
-  echo "Je viens du futur";
+    echo "insertion en BDD";
 }
 
-require 'frmContact.php';
+}
+else {
+  require 'frmContact.php';
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 date_default_timezone_set('Europe/Paris');
+require_once './functions/checkinput.php';
 
 require './includes/html.php';
 require './includes/header.php';
