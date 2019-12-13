e0c72f666028ebe8202442519a1de1c1afcb89ba
Christopher Lemaitre <kriss7670@gmail.com>
2019-12-13 12:11:28 +0100

    création frm inscription

---
+++ includes/frminscription.php
@@ -0,0 +1,13 @@
+<form action="index.php?page=inscription" method="post">
+
+<div>
+  <label for="email">e-mail</label>
+  <input type="text" name="mail">
+</div>
+<div>
+  <label for="pwd">mot de passe</label>
+  <input type="password" name="pwd">
+</div>
+<input type="submit" value="inscription">
+<input type="hidden" name="frminscription">
+</form>
